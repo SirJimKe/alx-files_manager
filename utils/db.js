a93f2c6b1928b65c0a46aae8f09eaf3dafd28a1c
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-03-13 05:27:37 +0300

    connect to mongodb

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    this.client.connect((error) => {
+      if (error) {
+	console.error('MongoDB connection error:', error);
+      } else {
+	console.log('Connected to MongoDB');
+      }
+    });
+
+    this.db = this.client.db(database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
